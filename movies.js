f8617757dd3255aa608d3ba19c73cfbbf92bb4b9
SiwooPak <bogus1620@naver.com>
2019-07-08 17:38:08 +0900

    all uploading

---
+++ movies.js
@@ -0,0 +1,22 @@
+const movies = [
+  {
+    "id":1,
+    "title": 'Avengers: End Game',
+    "desc": 'This movie is blabla',
+    "year": 2019
+  },
+  {
+    "id":2,
+    "title": 'Parasite',
+    "desc": 'This movie is Khan blabla',
+    "year": 2019
+  },
+  {
+    "id":3,
+    "title": 'Spiderman: Far from home',
+    "desc": 'This movie is blabla',
+    "year": 2019
+  }
+];
+
+module.exports = movies;
